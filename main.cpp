4287840094a8023919c703027eff7998036a1cf1
dalansol <160098081+dalansol@users.noreply.github.com>
2026-04-23 13:47:59 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,10 +25,12 @@ using namespace std;
 
 class Solution {
 public:
-    int gcdOfOddEvenSums(int n) {
+    int GcdOfOddEvenSums(int n) {
         int evenNumbers, oddNumbers = 0;
         evenNumbers = n*(n+1);
         oddNumbers = pow(n, 2);
         return gcd(evenNumbers, oddNumbers);
     }
 };
+
+
