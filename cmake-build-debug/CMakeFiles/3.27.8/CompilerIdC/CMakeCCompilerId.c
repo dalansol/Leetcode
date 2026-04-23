be9423e1a2522f6003e01e418e43fa4131cc2f46
dalansol <160098081+dalansol@users.noreply.github.com>
2026-04-23 13:37:11 -0600

    Update CMakeCCompilerId.c
    Add or update the header of this file

--- cmake-build-debug/CMakeFiles/3.27.8/CompilerIdC/CMakeCCompilerId.c
+++ cmake-build-debug/CMakeFiles/3.27.8/CompilerIdC/CMakeCCompilerId.c
@@ -1,3 +1,5 @@
+/* * <Your-Product-Name> * Copyright (c) <Year-From>-<Year-To> <Your-Company-Name> * * Please configure this header in your SonarCloud/SonarQube quality profile. * You can also set it in SonarLint.xml additional file for SonarLint or standalone NuGet analyzer. */
+
 #ifdef __cplusplus
 # error "A C++ compiler has been selected for C."
 #endif
